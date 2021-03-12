dc96f964a942fafe6f0f3d2a42c0a4be38c2cc07
Oliver <oliver_vukovic@yahoo.com>
2021-03-12 18:43:20 +0100

    new commit

--- src/main/java/main/car/Main.java
+++ src/main/java/main/car/Main.java
@@ -6,13 +6,15 @@ public class Main {
        int a = 5;
        int b = 6;
        Car audi = new Car();
+       audi.printAttributes();
        audi.brand = "Audi";
        audi.model = "Q3";
        audi.color = "white";
        //audi.BuildYear = 2012;
-       
+              
        System.out.println("Brand: " + audi.brand);
        System.out.println("Model: " + audi.model);
+       System.out.println("Color: " + audi.color);
        System.out.println("Godina proizvodnje: " + audi.getBuildYear());
         
         
@@ -24,10 +26,12 @@ public class Main {
        Car fiat = new Car(2010);
        System.out.println("Godina proizvodnje: " + fiat.getBuildYear());
    
-       Car lada = new Car("Lada", "Niva", "crvena", 2018);
+       Car lada = new Car("Lada", "Niva", "crvena", 2018, 0, 20, 5);
        lada.printAttributes();
-       
-       
+       lada.travel(100);
+       //lada.steFuel(100);
+       lada.printAttributes();
+              
     }
        
 }
